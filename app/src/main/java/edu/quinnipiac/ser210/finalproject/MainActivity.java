6de4b8a0df2993cd25275316434d9d350cc3a7f5
JamesJ182 <jjacobson@quinnipiac.edu>
2019-04-15 21:46:55 -0400

    Added Card Class

--- app/src/main/java/edu/quinnipiac/ser210/finalproject/MainActivity.java
+++ app/src/main/java/edu/quinnipiac/ser210/finalproject/MainActivity.java
@@ -1,21 +1,34 @@
 package edu.quinnipiac.ser210.finalproject;
 
+import android.content.Context;
 import android.net.Uri;
 import android.support.annotation.Nullable;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements ViewPager.OnPageChangeListener,
-        ShowSearchFragment.OnShowSearchListener,FavoritesFragment.OnFragmentInteractionListener {
+        ShowSearchFragment.OnShowSearchListener,FavoritesFragment.OnFragmentInteractionListener,ShowFragment.OnFragmentInteractionListener {
 
     private final static int SHOW_FRAGMENT=0;
     private final static int ACTOR_FRAGMENT=1;
     private final static int FAVORITES_FRAGMENT=2;
+    private static boolean isSearchingForShow;
+    private static boolean isSearchingForActor;
+    SectionsPagerAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,9 +36,11 @@ public class MainActivity extends AppCompatActivity implements ViewPager.OnPageC
         setContentView(R.layout.activity_main);
         TabLayout tabs=findViewById(R.id.tabs);
         ViewPager viewPager=findViewById(R.id.pager);
-        SectionsPagerAdapter adapter=new SectionsPagerAdapter(getSupportFragmentManager());
+        adapter=new SectionsPagerAdapter(getSupportFragmentManager());
         viewPager.setAdapter(adapter);
         tabs.setupWithViewPager(viewPager);
+        isSearchingForActor=true;
+        isSearchingForShow=true;
 
         //UberNova: "He's back"
         //James Jacobson is coolmanthegreat
@@ -53,7 +68,15 @@ public class MainActivity extends AppCompatActivity implements ViewPager.OnPageC
 
     @Override
     public void onClickShowSearch(String show) {
-        new FetchDetails(show).execute(true);
+        //new FetchDetails(show).execute(true);
+        //FragmentTransaction ft=getSupportFragmentManager().beginTransaction();
+        //ft.replace(R.layout.fragment_show_search,new ShowFragment());
+
+
+
+
+
+
     }
 
     @Override
@@ -78,13 +101,14 @@ public class MainActivity extends AppCompatActivity implements ViewPager.OnPageC
                     //return new ActorFragment();
                 case FAVORITES_FRAGMENT:
                     return new FavoritesFragment();
+
             }
             return null;
         }
 
         @Override
         public int getCount() {
-            return 2;
+            return 3;
         }
 
         @Nullable
---
+++ app/src/main/java/edu/quinnipiac/ser210/finalproject/ShowCard.java
@@ -0,0 +1,100 @@
+package edu.quinnipiac.ser210.finalproject;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.support.constraint.ConstraintLayout;
+import android.support.design.widget.TabLayout;
+import android.support.v7.widget.CardView;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+import java.io.InputStream;
+import java.net.URL;
+
+public class ShowCard{
+    private CardView card;
+    private TextView name;
+    private TextView rating;
+    private TextView status;
+    private ImageView image;
+    private Button button;
+    private TableLayout tableLayout;
+
+    public ShowCard (String name, String status, String imageURL, int rating, Context context)
+    {
+        this.name=new TextView(context);
+        this.name.setText(name);
+        this.rating=new TextView(context);
+        this.rating.setText(rating+"");
+        this.status=new TextView(context);
+        this.status.setText(status);
+        this.image=new ImageView(context);
+        new DownloadImageFromInternet(image).execute(imageURL);
+        button=new Button(context);
+        card=new CardView(context);
+        tableLayout=new TableLayout(context);
+        TableRow row1=new TableRow(context);
+        TableRow row2=new TableRow(context);
+        row1.addView(this.name);
+        row1.addView(this.image);
+        row2.addView(this.rating);
+        row2.addView(this.status);
+        row2.addView(this.button);
+        tableLayout.addView(row1);
+        tableLayout.addView(row2);
+        card.addView(tableLayout);
+    }
+
+    public TextView getName() {
+        return name;
+    }
+
+    public ImageView getImage() {
+        return image;
+    }
+
+    public TextView getRating() {
+        return rating;
+    }
+
+    public TextView getStatus() {
+        return status;
+    }
+
+    public CardView getCard() {
+        return card;
+    }
+
+    private class DownloadImageFromInternet extends AsyncTask<String, Void, Bitmap> {
+        ImageView imageView;
+
+        private DownloadImageFromInternet(ImageView imageView) {
+            this.imageView = imageView;
+        }
+
+        protected Bitmap doInBackground(String... urls) {
+            String imageURL = urls[0];
+            Bitmap bimage = null;
+            try {
+                InputStream in = new java.net.URL(imageURL).openStream();
+                bimage = BitmapFactory.decodeStream(in);
+
+            } catch (Exception e) {
+                Log.e("Error Message", e.getMessage());
+                e.printStackTrace();
+            }
+            return bimage;
+        }
+
+        protected void onPostExecute(Bitmap result) {
+            imageView.setImageBitmap(result);
+        }
+    }
+
+}
